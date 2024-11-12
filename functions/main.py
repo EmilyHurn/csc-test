51189ac4b7516bd21aa320f84e394d1a1e35e26e
Emily Hurn <90428953+EmilyHurn@users.noreply.github.com>
2024-11-12 12:41:27 -0500

    Added user input functionality

--- functions/main.py
+++ functions/main.py
@@ -1,14 +1,15 @@
 import hello
 import goodbye
 
+name = input("What is your name? ")
 # say hello when the user starts function
-hello.hello()
+hello.hello(name)
 
 # do some stuff
 i = 0
-while i < 10:
-    print("doing things")
+while i < len(name):
+    print(name[i])
     i += 1
 
 # say goodbye when the user ends function
-goodbye.goodbye()
+goodbye.goodbye(name)
